c717c5e70609063740b60042a12065aff3897b35
Oskari Saarimäki <oskari.antti.matti.saarimaki@cern.ch>
2019-08-13 15:51:47 +0300

    Added possibility to draw several different sets.
    To draw several different sets together, first you need to run several
    different sets in FinalMacro.C with different output names. Then set the
    names to PlotCentralityData.C and give readNFiles as how many sets you
    want to draw.

--- FinalMacro.C
+++ FinalMacro.C
@@ -1,6 +1,6 @@
 #include <TSystem.h>
 
-void FinalMacro(bool bPlotCentData=true, TString sIn="toyFlow.root") {
+void FinalMacro(bool bPlotCentData=true, TString sIn="toyFlow.root", TString sOut="toyFlowGraphs.root", int readNFiles=1) {
     gROOT->ProcessLine(".I src/ResIter.h");
 
     if (gSystem->AccessPathName(sIn)) {
@@ -9,13 +9,13 @@ void FinalMacro(bool bPlotCentData=true, TString sIn="toyFlow.root") {
     }
 
     if (bPlotCentData) {
-        cout << "Run MakeCentralityGraphs.C: " << Form(".x MakeCentralityGraphs.C(\"%s\")",sIn.Data()) << "\n";
-        gROOT->ProcessLine(Form(".x MakeCentralityGraphs.C(\"%s\")",sIn.Data()));
-        cout << "Run PlotCentralityData.C\n";
-        gROOT->ProcessLine(".x PlotCentralityData.C");
+        cout << "Run MakeCentralityGraphs.C: " << Form(".x MakeCentralityGraphs.C(\"%s\",\"%s\")",sIn.Data(),sOut.Data()) << "\n";
+        gROOT->ProcessLine(Form(".x MakeCentralityGraphs.C(\"%s\",\"%s\")",sIn.Data(),sOut.Data()));
+        cout << "Run PlotCentralityData.C: " << Form(".x PlotCentralityData.C(%i)",readNFiles) << "\n";
+        gROOT->ProcessLine(Form(".x PlotCentralityData.C(%i)",readNFiles));
     } else {
-        cout << "Run MakeGraphs.C: " << Form(".x MakeGraphs.C(\"%s\")",sIn.Data()) << "\n";
-        gROOT->ProcessLine(Form(".x MakeGraphs.C(\"%s\")",sIn.Data()));
+        cout << "Run MakeGraphs.C: " << Form(".x MakeGraphs.C(\"%s\",\"%s\")",sIn.Data(),sOut.Data()) << "\n";
+        gROOT->ProcessLine(Form(".x MakeGraphs.C(\"%s\",\"%s\")",sIn.Data(),sOut.Data()));
         cout << "Run PlotVn.C\n";
         gROOT->ProcessLine(".x PlotVn.C");
     }
--- PlotCentralityData.C
+++ PlotCentralityData.C
@@ -5,116 +5,148 @@
 
 void ErrorExit(TString error, int errnum=1 ){cout<<"ERROR: "<<error<<endl;gSystem->Exit(errnum);}
 
-TString fileName = "toyFlowCentralityGraphs.root";
+const int nFiles = 3;
+const int nRef = 0;
+TString fileName[nFiles] = {
+                            "toyFlowCentralityGraphs.root"
+                           ,"toyFlowCentralityGraphsGran.root"
+                           ,"toyFlowCentralityGraphsDoubleGran.root"
+                           };
+TString sSame[nFiles] = {"AP", "SAME P", "SAME P"};
 
-TH1D *hInputFlow[nCoef];
 
-TGraphErrors *gR[nCoef];
-TGraphErrors *gRtrue[nCoef];
-TGraphErrors *gVn[nCoef];
-TGraphErrors *gVnTrue[nCoef];
-TGraphErrors *gVnEP[nCoef];
-TGraphErrors *gVnSP[nCoef];
+// You can choose how many harmonics are drawn
+// by changing this number. Max=4
+const int nHarmonics = 2;
+int nXH[5] = {0, 1, 2, 2, 2};
+int nYH[5] = {0, 1, 1, 2, 2};
+int nXP[5] = {0, 500, 1000, 1000, 1000};
+int nYP[5] = {0, 500, 500, 1000, 1000};
+
+TH1D *hInputFlow[nCoef][nFiles];
+
+TGraphErrors *gR[nCoef][nFiles];
+TGraphErrors *gRtrue[nCoef][nFiles];
+TGraphErrors *gVn[nCoef][nFiles];
+TGraphErrors *gVnTrue[nCoef][nFiles];
+TGraphErrors *gVnEP[nCoef][nFiles];
+TGraphErrors *gVnSP[nCoef][nFiles];
 
 int mMarker = 24;
 double mSize = 1.0;
 
-TFile *fIn;
+TFile *fIn[nFiles];
 
-void PlotCentralityData() {
+void PlotCentralityData(int readNFiles = 1) {
 
-    fIn = TFile::Open(fileName, "read");
+    for(int iFil=0; iFil<readNFiles; iFil++)
+        fIn[iFil] = TFile::Open(fileName[iFil], "read");
 
     int i, j;
-    for (i=0; i<nCoef; i++) {
-        if(fIn==0) ErrorExit(Form("Cannot open file: %s",fileName.Data()));
+    for(int iFil=0; iFil<readNFiles; iFil++) {
+        for (i=0; i<nCoef; i++) {
+            if(fIn[iFil]==0) ErrorExit(Form("Cannot open file: %s",fileName[iFil].Data()));
 
-        hInputFlow[i] = (TH1D*) fIn->Get(Form("hInputFlow%02i", i+1));
+            hInputFlow[i][iFil] = (TH1D*) fIn[iFil]->Get(Form("hInputFlow%02i", i+1));
 
-        gR[i] = (TGraphErrors*) fIn->Get(Form("gRH%02i", i+1));
-        gRtrue[i] = (TGraphErrors*) fIn->Get(Form("gRtrueH%02i", i+1));
+            gR[i][iFil] = (TGraphErrors*) fIn[iFil]->Get(Form("gRH%02i", i+1));
+            gRtrue[i][iFil] = (TGraphErrors*) fIn[iFil]->Get(Form("gRtrueH%02i", i+1));
 
-        gVn[i] = (TGraphErrors*) fIn->Get(Form("gVnH%02i", i+1));
-        gVnTrue[i] = (TGraphErrors*) fIn->Get(Form("gVnTrueH%02i", i+1));
-        gVnEP[i] = (TGraphErrors*) fIn->Get(Form("gVnEPH%02i", i+1));
-        gVnSP[i] = (TGraphErrors*) fIn->Get(Form("gVnSPH%02i", i+1));
+            gVn[i][iFil] = (TGraphErrors*) fIn[iFil]->Get(Form("gVnH%02i", i+1));
+            gVnTrue[i][iFil] = (TGraphErrors*) fIn[iFil]->Get(Form("gVnTrueH%02i", i+1));
+            gVnEP[i][iFil] = (TGraphErrors*) fIn[iFil]->Get(Form("gVnEPH%02i", i+1));
+            gVnSP[i][iFil] = (TGraphErrors*) fIn[iFil]->Get(Form("gVnSPH%02i", i+1));
 
-        gR[i]->SetMarkerStyle(mMarker);
-        gR[i]->SetMarkerColor(1);
-        gR[i]->SetMarkerSize(mSize);
+            gR[i][iFil]->SetMarkerStyle(mMarker);
+            gR[i][iFil]->SetMarkerColor(1+iFil);
+            gR[i][iFil]->SetMarkerSize(mSize);
+            gR[i][iFil]->SetFillColor(1+iFil);
 
-        gRtrue[i]->SetMarkerStyle(mMarker+1);
-        gRtrue[i]->SetMarkerColor(1);
-        gRtrue[i]->SetMarkerSize(mSize);
+            gRtrue[i][iFil]->SetMarkerStyle(mMarker+1);
+            gRtrue[i][iFil]->SetMarkerColor(1+iFil);
+            gRtrue[i][iFil]->SetMarkerSize(mSize);
+            gRtrue[i][iFil]->SetFillColor(1+iFil);
 
-        gVn[i]->SetMarkerStyle(mMarker);
-        gVn[i]->SetMarkerColor(1);
-        gVn[i]->SetMarkerSize(mSize);
+            gVn[i][iFil]->SetMarkerStyle(mMarker);
+            gVn[i][iFil]->SetMarkerColor(1+iFil);
+            gVn[i][iFil]->SetMarkerSize(mSize);
+            gVn[i][iFil]->SetFillColor(1+iFil);
 
-        gVnTrue[i]->SetMarkerStyle(mMarker+1);
-        gVnTrue[i]->SetMarkerColor(1);
-        gVnTrue[i]->SetMarkerSize(mSize);
+            gVnTrue[i][iFil]->SetMarkerStyle(mMarker+1);
+            gVnTrue[i][iFil]->SetMarkerColor(1+iFil);
+            gVnTrue[i][iFil]->SetMarkerSize(mSize);
+            gVnTrue[i][iFil]->SetFillColor(1+iFil);
 
-        gVnEP[i]->SetMarkerStyle(mMarker+2);
-        gVnEP[i]->SetMarkerColor(1);
-        gVnEP[i]->SetMarkerSize(mSize);
+            gVnEP[i][iFil]->SetMarkerStyle(mMarker+3);
+            gVnEP[i][iFil]->SetMarkerColor(1+iFil);
+            gVnEP[i][iFil]->SetMarkerSize(mSize);
+            gVnEP[i][iFil]->SetFillColor(1+iFil);
 
-        gVnSP[i]->SetMarkerStyle(mMarker+4);
-        gVnSP[i]->SetMarkerColor(1);
-        gVnSP[i]->SetMarkerSize(mSize);
-
+            gVnSP[i][iFil]->SetMarkerStyle(mMarker+4);
+            gVnSP[i][iFil]->SetMarkerColor(1+iFil);
+            gVnSP[i][iFil]->SetMarkerSize(mSize);
+            gVnSP[i][iFil]->SetFillColor(1+iFil);
+        }
     }
 
     gStyle->SetOptStat(0);
 
-    TCanvas *c1 = new TCanvas("c1", "c1");
-    c1->Divide(2,2);
+    TCanvas *c1 = new TCanvas("c1", "c1", nXP[nHarmonics], nYP[nHarmonics]);
+    c1->Divide(nXH[nHarmonics],nYH[nHarmonics]);
 
-    for (i=1; i<nCoef; i++) {
-        c1->cd(i);
-        gVn[i]->Draw("AP");
-        gVn[i]->GetYaxis()->SetRangeUser(0.0,0.18);
-        gVn[i]->GetXaxis()->CenterTitle(true);
-        gVn[i]->GetYaxis()->CenterTitle(true);
-        gVn[i]->SetTitle(Form("n=%01i; centrality; v_{n}", i+1));
-        gVn[i]->Draw("AP");
-        gVnTrue[i]->Draw("SAME P");
-        gVnEP[i]->Draw("SAME P");
-        gVnSP[i]->Draw("SAME P");
-        hInputFlow[i]->Draw("SAME HIST");
-        c1->Update();
+    for (i=1; i<nHarmonics+1; i++) {
+        for(int iFil=0; iFil<readNFiles; iFil++) {
+            c1->cd(i);
+            gVn[i][iFil]->GetYaxis()->SetRangeUser(0.0,0.11);
+            gVn[i][iFil]->GetXaxis()->CenterTitle(true);
+            gVn[i][iFil]->GetYaxis()->CenterTitle(true);
+            gVn[i][iFil]->SetTitle(Form("n=%01i; centrality; v_{n}", i+1));
+            gVn[i][iFil]->Draw(Form("%s",sSame[iFil].Data()));
+            gVnTrue[i][iFil]->Draw("SAME P");
+            gVnEP[i][iFil]->Draw("SAME P");
+            gVnSP[i][iFil]->Draw("SAME P");
+            hInputFlow[i][iFil]->Draw("SAME HIST");
+            c1->Update();
+        }
     }
 
-    TLegend *leg1 = new TLegend(0.50,0.6,0.85,0.85,"","brNDC");
+    TLegend *leg1 = new TLegend(0.30,0.5,0.55,0.85,"","brNDC");
     leg1->SetTextSize(0.037);leg1->SetBorderSize(0);
 
-    leg1->AddEntry(gVnTrue[0], "v_{n}, true RP", "p");
-    leg1->AddEntry(gVn[0], "v_{n}, trad. EP", "p");
-    leg1->AddEntry(gVnEP[0], "v_{n}{EP}", "p");
-    leg1->AddEntry(gVnSP[0], "v_{n}{SP}", "p");
+    leg1->AddEntry(gVnTrue[0][nRef], "v_{n}, true RP", "p");
+    leg1->AddEntry(gVn[0][nRef], "v_{n}, trad. EP", "p");
+    leg1->AddEntry(gVnEP[0][nRef], "v_{n}{EP}", "p");
+    leg1->AddEntry(gVnSP[0][nRef], "v_{n}{SP}", "p");
+    if(readNFiles>1) leg1->AddEntry(gR[0][0], "Ideal detector", "f");
+    if(readNFiles>1) leg1->AddEntry(gR[0][1], "8 sec granular detector", "f");
+    if(readNFiles>2) leg1->AddEntry(gR[0][2], "16 sec granular detector", "f");
     leg1->Draw("SAME");
     c1->Draw();
 
-    TCanvas *c2 = new TCanvas("c2", "c2");
-    c2->Divide(2,2);
+    TCanvas *c2 = new TCanvas("c2", "c2", nXP[nHarmonics], nYP[nHarmonics]);
+    c2->Divide(nXH[nHarmonics],nYH[nHarmonics]);
 
-    for (i=1; i<nCoef; i++) {
-        c2->cd(i);
-        gR[i]->Draw("AP");
-        gR[i]->GetYaxis()->SetRangeUser(0.0,1.1);
-        gR[i]->GetXaxis()->CenterTitle(true);
-        gR[i]->GetYaxis()->CenterTitle(true);
-        gR[i]->SetTitle(Form("n=%01i; centrality; R_{n}", i+1));
-        gR[i]->Draw("AP");
-        gRtrue[i]->Draw("SAME P");
-        c2->Update();
+    for (i=1; i<nHarmonics+1; i++) {
+        for(int iFil=0; iFil<readNFiles; iFil++) {
+            c2->cd(i);
+            gR[i][iFil]->GetYaxis()->SetRangeUser(0.35,1.1);
+            gR[i][iFil]->GetXaxis()->CenterTitle(true);
+            gR[i][iFil]->GetYaxis()->CenterTitle(true);
+            gR[i][iFil]->SetTitle(Form("n=%01i; centrality; R_{n}", i+1));
+            gR[i][iFil]->Draw(Form("%s",sSame[iFil].Data()));
+            gRtrue[i][iFil]->Draw("SAME P");
+            c2->Update();
+        }
     }
 
-    TLegend *leg2 = new TLegend(0.50,0.6,0.85,0.85,"","brNDC");
+    TLegend *leg2 = new TLegend(0.30,0.7,0.55,0.85,"","brNDC");
     leg2->SetTextSize(0.037);leg2->SetBorderSize(0);
 
-    leg2->AddEntry(gRtrue[0], "R true, uniform #phi", "p");
-    leg2->AddEntry(gR[0], "R sub event method, uniform #phi", "p");
+    leg2->AddEntry(gRtrue[0][nRef], "R true, uniform #phi", "p");
+    leg2->AddEntry(gR[0][nRef], "R sub event method, uniform #phi", "p");
+    if(readNFiles>1) leg2->AddEntry(gR[0][0], "Ideal detector", "f");
+    if(readNFiles>1) leg2->AddEntry(gR[0][1], "8 sec granular detector", "f");
+    if(readNFiles>2) leg2->AddEntry(gR[0][2], "16 sec granular detector", "f");
     leg2->Draw("SAME");
     c2->Draw();
 
